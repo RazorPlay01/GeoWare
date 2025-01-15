4aa9a8775c18ad792f5767138d52d6f1e7fe89b4
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2025-01-15 01:19:43 -0500

    🌱 Changelog: - Added texture reference - Added barrels and debug utils
    🐞 Known Bugs:
    -
    
    📋 Backlog:
    -

---
+++ DonkeyKongFabric/src/main/java/com/github/razorplay01/donkeykongfabric/game/DebugPoint.java
@@ -0,0 +1,12 @@
+package com.github.razorplay01.donkeykongfabric.game;
+
+public class DebugPoint {
+    float x, y;
+    int color;
+
+    DebugPoint(float x, float y, int color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
