294515d9110c5fde04873b97b7d1afa7838ae7db
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2025-01-21 02:38:11 -0500

    🌱 Changelog: - Added new textures - Refactor
    🐞 Known Bugs:
    -
    
    📋 Backlog:
    -

--- DonkeyKongFabric/src/main/java/com/github/razorplay01/donkeykongfabric/game/mapobject/Platform.java
+++ DonkeyKongFabric/src/main/java/com/github/razorplay01/donkeykongfabric/game/mapobject/Platform.java
@@ -1,8 +1,5 @@
 package com.github.razorplay01.donkeykongfabric.game.mapobject;
 
-import lombok.Getter;
-
-@Getter
 public class Platform extends MapObject {
     public Platform(float xPos, float yPos, float width, float height) {
         super(xPos, yPos, width, height, 0xAAcf6800);
