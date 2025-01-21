f0ff333f2146ac5e30bdf2b7c564f2ba898e9203
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2025-01-21 02:32:41 -0500

    🌱 Changelog: - Added VictoryZone
    🐞 Known Bugs:
    -
    
    📋 Backlog:
    -

---
+++ DonkeyKongFabric/src/main/java/com/github/razorplay01/donkeykongfabric/game/mapobject/VictoryZone.java
@@ -0,0 +1,7 @@
+package com.github.razorplay01.donkeykongfabric.game.mapobject;
+
+public class VictoryZone extends MapObject {
+    public VictoryZone(float xPos, float yPos, float width, float height, int debugColor) {
+        super(xPos, yPos, width, height, debugColor);
+    }
+}
