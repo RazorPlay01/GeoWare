648c7495849f992a3cb583acf2646702a9cf259f
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2025-03-13 17:04:02 -0400

    🌱 Changelog: # ModBubblePuzzle - A working version of BubblePuzzle has been completed.
    🐞 Known Bugs:
    -
    
    📋 Backlog:
    -

---
+++ ModBubblePuzzle/src/main/java/com/github/razorplay01/modbubblepuzzle/ModBubblePuzzle.java
@@ -0,0 +1,33 @@
+package com.github.razorplay01.modbubblepuzzle;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.command.CommandManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModBubblePuzzle implements ModInitializer, ClientModInitializer {
+    public static final String MOD_ID = "bubblepuzzle";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final boolean IS_DEBUG_MODE_ENABLE = false;
+
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("bubblepuzzle")
+                    .executes(context -> {
+                        if (context.getSource().getPlayer() != null) {
+                            MinecraftClient.getInstance().execute(() -> MinecraftClient.getInstance().setScreen(new BubblePuzzleScreen()));
+                        }
+                        return 1;
+                    }));
+        });
+    }
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
