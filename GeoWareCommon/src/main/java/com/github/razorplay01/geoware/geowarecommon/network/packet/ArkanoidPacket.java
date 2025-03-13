3a8ae3f53ec2052da8e731ccb5e46f9cbd489719
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2025-03-13 17:02:22 -0400

    🌱 Changelog: # ModArkanoid - Packet system for Arkanoid
    🐞 Known Bugs:
    -
    
    📋 Backlog:
    -

---
+++ GeoWareCommon/src/main/java/com/github/razorplay01/geoware/geowarecommon/network/packet/ArkanoidPacket.java
@@ -0,0 +1,40 @@
+package com.github.razorplay01.geoware.geowarecommon.network.packet;
+
+import com.github.razorplay01.geoware.geowarecommon.exceptions.PacketSerializationException;
+import com.github.razorplay01.geoware.geowarecommon.network.IPacket;
+import com.github.razorplay01.geoware.geowarecommon.network.Packet;
+import com.github.razorplay01.geoware.geowarecommon.network.network_util.PacketDataSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Packet
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArkanoidPacket implements IPacket {
+    private int score;
+    private int timeLimitSeconds;
+    private int level;
+
+    @Override
+    public void read(PacketDataSerializer serializer) throws PacketSerializationException {
+        this.score = serializer.readInt();
+        this.timeLimitSeconds = serializer.readInt();
+        this.level = serializer.readInt();
+    }
+
+    @Override
+    public void write(PacketDataSerializer serializer) throws PacketSerializationException {
+        serializer.writeInt(this.score);
+        serializer.writeInt(this.timeLimitSeconds);
+        serializer.writeInt(this.level);
+    }
+
+
+    @Override
+    public String getPacketId() {
+        return "ArkanoidPacket";
+    }
+}
